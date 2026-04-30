613c714e4fe66ac45fb3b12be68ccc666b6cfb93
cvwellons <cvwellons@liberty.edu>
2026-04-29 23:21:21 -0700

    Implemented edgeCount and writeToFile functions in graph.cpp.

--- Programming-Assignment-5/graph.cpp
+++ Programming-Assignment-5/graph.cpp
@@ -13,7 +13,7 @@ Graph::Graph(int vertices) : n(vertices), mat(vertices, std::vector<int>(vertice
 
 // Member functions
 bool Graph::readFromFile(const std::string& filename) { // Reads graph from a file
-    std::ifstream in(filename); // Open file for reading
+    std::ifstream in(filename);
 
     if (!in) { // If file did not open
         return false;
@@ -22,18 +22,47 @@ bool Graph::readFromFile(const std::string& filename) { // Reads graph from a fi
         return false;
     }
 
-    mat.assign(n, std::vector<int>(n, 0)); // Resize matrix to n x n and initialize with zeros
+    mat.assign(n, std::vector<int>(n, 0)); // Resize and initialize with zeros
 
     for (int i = 0; i < n; ++i) { // For each row
         for (int j = 0; j < n; ++j) { // For each column
-            if (!(in >> mat[i][j])) { // If reading matrix element failed
+            if (!(in >> mat[i][j])) { // If reading fails
                 return false;
             }
         }
     }
     return true;
 }
-void Graph::printToConsole() const { // Prints graph to console
+bool Graph::writeToFile(const std::string& filename) const { // Writes graph to file
+    std::ofstream out(filename); // Open file
+
+    if (!out) { // If file did not open
+        return false;
+    }
+
+    out << n << std::endl; // Write number of vertices
+
+    for (int i = 0; i < n; ++i) { // For each row
+        for (int j = 0; j < n; ++j) { // For each column
+            out << mat[i][j] << (j + 1 == n ? "" : " "); // Write element with space except for last
+        }
+        out << std::endl;
+    }
+    return true;
+}
+int Graph::edgeCount() const { // Number of edges in the graph
+    int count = 0;
+    for (int i = 0; i < n; ++i) { // For each row
+        for (int j = i + 1; j < n; ++j) { // For each column after the diagonal
+            if (mat[i][j] != 0) { // If there is an edge
+                ++count;
+            }
+        }
+    }
+
+    return count;
+}
+void Graph::printToConsole() const { // Print to console
     std::cout << "Number of vertices is " << n << std::endl;
     std::cout << "Number of edges is " << edgeCount() << std::endl;
     std::cout << "Matrix is:" << std::endl;
