54d58ffb6c091d7ee5cf133e2f7d02a36eafd7d7
cvwellons <cvwellons@liberty.edu>
2026-04-29 23:24:42 -0700

    Added set/get functions to graph.h and private member variables

--- Programming-Assignment-5/graph.h
+++ Programming-Assignment-5/graph.h
@@ -14,8 +14,15 @@ public:
 	int edgeCount() const; // Number of edges
 	void printToConsole() const;
 
+	// Set functions
+	void setEdge(int i, int j, int weight); // Sets weight of the edge
+
+	// Get functions
+	int getVertexCount() const; // Number of vertices
+	int getEdge(int i, int j) const; // Weight of the edge
+	const std::vector<std::vector<int>>& getMatrix() const; // Matrix
+
 private:
-	// Variables
 	int n; // Number of vertices
-	std::vector<std::vector<int>> mat; // Adjacency matrix
+	std::vector<std::vector<int>> mat; // Matrix
 };
